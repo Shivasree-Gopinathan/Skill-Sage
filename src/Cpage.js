3e2974e58f99fac051f3f2a11bdcbd8643d79170
JanviKPat <janvipatel@janvis-air.home>
2023-07-22 13:59:32 -0400

    Community changes

--- src/Cpage.js
+++ src/Cpage.js
@@ -1,85 +1,218 @@
-import React,{useState} from "react";
-import{ScrollView,View,Text,Statusbar, Dimensions, Image} from 'react-native';
-import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet } from "react-native";
+import React from 'react';
+import {
+  SafeAreaView,
+  Image,
+  StyleSheet,
+  FlatList,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
 
-const image=[
-  'https://img.freepik.com/free-vector/flat-university-concept_23-2148176380.jpg?w=1380&t=st=1689970064~exp=1689970664~hmac=5a5ae5faea445eed8af2a3fd7b79d2f52b50daea16b303189bc25ab3f3181e7d',
-  'https://img.freepik.com/free-vector/woman-giving-comfort-support-friend_74855-5301.jpg?w=2000&t=st=1689970253~exp=1689970853~hmac=6e688d396fdbb60dc9f7b43fbc4aeb8426ebe3d10445b56248812be57bff1116',
-  'https://img.freepik.com/free-vector/family-couple-saving-money_74855-5240.jpg?w=2000&t=st=1689970407~exp=1689971007~hmac=848db56e3fdfdb3cd77e158fdb182b941997eb7f582b9ec6cdfac9c239aed56d',
-]
+const {width, height} = Dimensions.get('window');
 
-const WIDTH = Dimensions.get('window').width;
-const HEIGHT = Dimensions.get('window').height;
+const COLORS = {primary: '#282534', white: '#fff'};
+
 export default function Cpage () {
-  const [imagActive, setimgActive] = useState(0);
+const slides = [
+  {
+    id: '1',
+    image: require('../images/image1.png'),
+    title: 'Best Digital Solution',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    id: '2',
+    image: require('../images/image2.png'),
+    title: 'Achieve Your Goals',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    id: '3',
+    image: require('../images/image3.png'),
+    title: 'Increase Your Value',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+];
 
-  onchange = (nativeEvent) => {
-      if(nativeEvent){
-        const slide = Math.ceil(nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width);
-        if(slide != imagActive) {
-          setimgActive(slide);
-        }
-      }
-  }
+const Slide = ({item}) => {
   return (
-   <SafeAreaView style={styles.container}>
-    <View style={styles.wrap}>
-      <ScrollView onScroll={({nativeEvent}) => onchange(nativeEvent)}
-        showsHorizontalScrollIndicator = {false}
-        pagingEnabled
-        horizontal
-        style={styles.wrap}>
-          {
-          image.map((e, index) => 
-          <Image
-          Key={e}
-          resizeMode='stretch'
-          style={styles.wrap}
-          source={{uri: e}}>
-              
-
-          </Image>)
-          }
-        
-      </ScrollView>
-      <View style={styles.wrapDot}>
-          {
-            image.map((e, index) => 
-            <Text
-              key= {e}
-              style={imagActive == index ? styles.dotActive : styles.dot}>
-              ●
-
-            </Text>)
-          }
+    <View style={{alignItems: 'center'}}>
+      <Image
+        source={item?.image}
+        style={{height: '75%', width, resizeMode: 'contain'}}
+      />
+      <View>
+        <Text style={styles.title}>{item?.title}</Text>
+        <Text style={styles.subtitle}>{item?.subtitle}</Text>
       </View>
     </View>
-   </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex:1
-  },
-  wrap:{
-    width:WIDTH,
-    height: HEIGHT * 0.75
-  },
-  wrapDot:{
-    position : "absolute",
-    bottom: 0,
-    flexDirection:'row',
-    alignSelf:'center'
+const OnboardingScreen = ({navigation}) => {
+  const [currentSlideIndex, setCurrentSlideIndex] = React.useState(0);
+  const ref = React.useRef();
+  const updateCurrentSlideIndex = e => {
+    const contentOffsetX = e.nativeEvent.contentOffset.x;
+    const currentIndex = Math.round(contentOffsetX / width);
+    setCurrentSlideIndex(currentIndex);
+  };
 
+  const goToNextSlide = () => {
+    const nextSlideIndex = currentSlideIndex + 1;
+    if (nextSlideIndex != slides.length) {
+      const offset = nextSlideIndex * width;
+      ref?.current.scrollToOffset({offset});
+      setCurrentSlideIndex(currentSlideIndex + 1);
+    }
+  };
+
+  const skip = () => {
+    const lastSlideIndex = slides.length - 1;
+    const offset = lastSlideIndex * width;
+    ref?.current.scrollToOffset({offset});
+    setCurrentSlideIndex(lastSlideIndex);
+  };
+
+  const Footer = () => {
+    return (
+      <View
+        style={{
+          height: height * 0.25,
+          justifyContent: 'space-between',
+          paddingHorizontal: 20,
+        }}>
+        {/* Indicator container */}
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginTop: 20,
+          }}>
+          {/* Render indicator */}
+          {slides.map((_, index) => (
+            <View
+              key={index}
+              style={[
+                styles.indicator,
+                currentSlideIndex == index && {
+                  backgroundColor: COLORS.white,
+                  width: 25,
+                },
+              ]}
+            />
+          ))}
+        </View>
+
+        {/* Render buttons */}
+        <View style={{marginBottom: 20}}>
+          {currentSlideIndex == slides.length - 1 ? (
+            <View style={{height: 50}}>
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={() => navigation.replace('HomeScreen')}>
+                <Text style={{fontWeight: 'bold', fontSize: 15}}>
+                  GET STARTED
+                </Text>
+              </TouchableOpacity>
+            </View>
+          ) : (
+            <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                style={[
+                  styles.btn,
+                  {
+                    borderColor: COLORS.white,
+                    borderWidth: 1,
+                    backgroundColor: 'transparent',
+                  },
+                ]}
+                onPress={skip}>
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                    fontSize: 15,
+                    color: COLORS.white,
+                  }}>
+                  SKIP
+                </Text>
+              </TouchableOpacity>
+              <View style={{width: 15}} />
+              <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={goToNextSlide}
+                style={styles.btn}>
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                    fontSize: 15,
+                  }}>
+                  NEXT
+                </Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.primary}}>
+      <StatusBar backgroundColor={COLORS.primary} />
+      <FlatList
+        ref={ref}
+        onMomentumScrollEnd={updateCurrentSlideIndex}
+        contentContainerStyle={{height: height * 0.75}}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        data={slides}
+        pagingEnabled
+        renderItem={({item}) => <Slide item={item} />}
+      />
+      <Footer />
+    </SafeAreaView>
+  );
+}};
+
+const styles = StyleSheet.create({
+  subtitle: {
+    color: COLORS.white,
+    fontSize: 13,
+    marginTop: 10,
+    maxWidth: '70%',
+    textAlign: 'center',
+    lineHeight: 23,
   },
-  dotActive :{
-    margin: 3,
-    color:'black'
+  title: {
+    color: COLORS.white,
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 20,
+    textAlign: 'center',
   },
-  dot:{
-    margin: 3,
-    color: 'white'
-  }
+  image: {
+    height: '100%',
+    width: '100%',
+    resizeMode: 'contain',
+  },
+  indicator: {
+    height: 2.5,
+    width: 10,
+    backgroundColor: 'grey',
+    marginHorizontal: 3,
+    borderRadius: 2,
+  },
+  btn: {
+    flex: 1,
+    height: 50,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
