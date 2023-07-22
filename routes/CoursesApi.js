32b909f110c9ae2e38545529975f1acccc268504
gargirp <135798162+gargirp@users.noreply.github.com>
2023-07-22 15:47:34 -0400

    Addition of course content

--- routes/CoursesApi.js
+++ routes/CoursesApi.js
@@ -32,8 +32,8 @@ const Courses = [
     title: 'Data Structures and Algorithm',
     image: require('../assets/course4.jpg'),
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor ante dui, sed ornare augue porta id. Curabitur molestie a diam in malesuada. Vivamus rhoncus mi accumsan, viverra sem nec, mattis elit.',
-    course1: 'html',
+      'Data structure is a data organization, management, and storage format that is usually chosen for efficient access to data. More precisely, a data structure is a collection of data values, the relationships among them, and the functions or operations that can be applied to the data, it is an algebraic structure about data.',
+    course1: 'Data structures are generally based on the ability of a computer to fetch and store data at any place in its memory, specified by a pointer—a bit string, representing a memory address, that can be itself stored in memory and manipulated by the program. Thus, the array and record data structures are based on computing the addresses of data items with arithmetic operations, while the linked data structures are based on storing addresses of data items within the structure itself. Data structures serve as the basis for abstract data types (ADT). The ADT defines the logical form of the data type. The data structure implements the physical form of the data type.Different types of data structures are suited to different kinds of applications, and some are highly specialized to specific tasks. For example, relational databases commonly use B-tree indexes for data retrieval, while compiler implementations usually use hash tables to look up identifiers',
     price: 1000,
   },
   {
