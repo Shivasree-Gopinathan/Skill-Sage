1a05c02337eae9578bac232b3e413ed0efaee4ec
gargirp <135798162+gargirp@users.noreply.github.com>
2023-07-22 15:37:44 -0400

    Addition of content

--- routes/CoursesApi.js
+++ routes/CoursesApi.js
@@ -23,9 +23,8 @@ const Courses = [
     id: '3',
     title: 'Machine Learning',
     image: require('../assets/course3.jpg'),
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor ante dui, sed ornare augue porta id. Curabitur molestie a diam in malesuada. Vivamus rhoncus mi accumsan, viverra sem nec, mattis elit.',
-    course1: 'html',
+    description: 'Machine learning (ML) is an umbrella term for solving problems for which development of algorithms by human programmers would be cost-prohibitive, and instead the problems are solved by helping machines',
+    course1: 'Modern-day machine learning has two objectives, one is to classify data based on models which have been developed, the other purpose is to make predictions for future outcomes based on these models. A hypothetical algorithm specific to classifying data may use computer vision of moles coupled with supervised learning in order to train it to classify the cancerous moles. A machine learning algorithm for stock trading may inform the trader of future potential predictions. Performing machine learning can involve creating a model, which is trained on some training data and then can process additional data to make predictions. Various types of models have been used and researched for machine learning systems.',
     price: 1000,
   },
   {
