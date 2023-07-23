fe2f1a6d75f78c3dbf18c1b89873fd3de2be62d1
gargirp <135798162+gargirp@users.noreply.github.com>
2023-07-23 15:27:40 -0400

    Updates in Courses

--- routes/CoursesApi.js
+++ routes/CoursesApi.js
@@ -46,9 +46,9 @@ const Courses = [
     image: require('../assets/course4.jpg'),
     description:
       'Data structure is a data organization, management, and storage format that is usually chosen for efficient access to data. More precisely, a data structure is a collection of data values, the relationships among them, and the functions or operations that can be applied to the data, it is an algebraic structure about data.',
-    course1: ' Database Foundations: Data Structures',
-    course1: ' Data Structures and Algorithms using Java',
-    course1: ' Data Structures and Algorithms using Python',
+    course1: '1) Database Foundations: Data Structures',
+    course2: '2) Data Structures and Algorithms using Java',
+    course3: '3) Data Structures and Algorithms using Python',
     price: 1000,
   },
   {
@@ -57,7 +57,7 @@ const Courses = [
     image: require('../assets/course5.png'),
     description:
       'Web development is the work involved in developing a website for the Internet (World Wide Web) or an intranet (a private network).Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services.',
-    course1: 'Web development is the process of building websites and applications for the internet, or for a private network known as an intranet.Web development can be broken down into three layers: client-side coding (frontend), server-side coding (backend) and database technology.Client-side scripting, or frontend development, refers to everything that the end user experiences directly.Server-side scripting, or backend development, is all about what goes on behind the scenes.The backend is essentially the part of a website that the user doesn’t actually see. It is responsible for storing and organizing data, and ensuring that everything on the client-side runs smoothly. It does this by communicating with the frontend.The database contains all the files and content that are necessary for a website to function, storing it in such a way that makes it easy to retrieve, organize, edit, and save.',
+    course1: '',
     price: 1000,
   },
 ]
